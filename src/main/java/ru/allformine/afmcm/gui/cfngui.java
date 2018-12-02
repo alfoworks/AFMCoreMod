05e09642efc13e41e01409a31af3176c577365f5
Iterator <rob.bog@list.ru>
2018-12-02 18:52:58 +0300

    хуй

--- src/main/java/ru/allformine/afmcm/gui/cfngui.java
+++ src/main/java/ru/allformine/afmcm/gui/cfngui.java
@@ -30,8 +30,8 @@ public class cfngui
     
     String s = "[" + this.text + EnumChatFormatting.WHITE + "]";
     this.mc.fontRenderer.drawStringWithShadow(s,
-            200 - this.mc.fontRenderer.getStringWidth(s),
-            height - this.mc.fontRenderer.FONT_HEIGHT,
+            0,
+            height - this.mc.fontRenderer.FONT_HEIGHT - 5,
             16777215);
   }
 }
