25a848512ecb7bbfd4773a4be3e7d553a020debc
Rob <rob.bog@list.ru>
2019-01-29 19:54:36 +0300

    Разделяем массив на 2, иначе размер пакета слишком большой :/

--- src/main/java/ru/allformine/afmcm/proxy/ScreenshotProxy.java
+++ src/main/java/ru/allformine/afmcm/proxy/ScreenshotProxy.java
@@ -8,17 +8,29 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.network.play.client.C17PacketCustomPayload;
 import ru.allformine.afmcm.screenshot.ScreenshotMaker;
 
+import java.util.Arrays;
+
 public class ScreenshotProxy {
 
     @SubscribeEvent
     public void onClientPacket(FMLNetworkEvent.ClientCustomPacketEvent event) {
         Minecraft mc = Minecraft.getMinecraft();
         byte[] image = ScreenshotMaker.getScreenshotByteArray(mc.displayWidth, mc.displayHeight, mc.getFramebuffer());
-        ByteBuf buf = Unpooled.buffer(0);
 
-        buf.writeBytes(image);
+        if (image != null) {
+            byte[][] dividedImages = new byte[2][];
 
-        C17PacketCustomPayload packet = new C17PacketCustomPayload("scr", buf);
-        mc.thePlayer.sendQueue.addToSendQueue(packet);
+            dividedImages[0] = Arrays.copyOfRange(image, 0, image.length / 2);
+            dividedImages[1] = Arrays.copyOfRange(image, image.length / 2, image.length);
+
+            for (byte[] dividedImage : dividedImages) {
+                ByteBuf buf = Unpooled.buffer(0);
+
+                buf.writeBytes(dividedImage);
+
+                C17PacketCustomPayload packet = new C17PacketCustomPayload("scr", buf);
+                mc.thePlayer.sendQueue.addToSendQueue(packet);
+            }
+        }
     }
 }
