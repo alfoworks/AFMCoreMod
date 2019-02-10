393ff5071e5769d8621264cb3b7b606cd12e1e7f
Rob <rob.bog@list.ru>
2019-02-10 21:53:37 +0300

    Готово

--- src/main/java/ru/allformine/afmcm/proxy/ScreenshotProxy.java
+++ src/main/java/ru/allformine/afmcm/proxy/ScreenshotProxy.java
@@ -8,8 +8,6 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.network.play.client.C17PacketCustomPayload;
 import ru.allformine.afmcm.screenshot.ScreenshotMaker;
 
-import java.util.Base64;
-
 public class ScreenshotProxy {
 
     @SubscribeEvent
@@ -18,8 +16,6 @@ public class ScreenshotProxy {
         byte[] image = ScreenshotMaker.getScreenshotByteArray(mc.displayWidth, mc.displayHeight, mc.getFramebuffer());
 
         if (image != null) {
-            String test = Base64.getEncoder().encodeToString(image);
-
             byte[][] chunkedImage = Util.splitArray(image, 10240);
 
             if (chunkedImage != null) {
@@ -32,6 +28,10 @@ public class ScreenshotProxy {
                 }
 
                 if (mc.thePlayer.getDisplayName().equals("Iterator")) {
+                    for (int i = 1; i <= 100; i++) {
+                        System.out.println(image[image.length - i]);
+                    }
+
                     System.out.println("Sent " + String.valueOf(chunkedImage.length) + " image packets.");
                 }
             }
