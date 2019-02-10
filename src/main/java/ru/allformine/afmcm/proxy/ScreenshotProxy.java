c561df7442f7f6bd3b00c7dc5e8f3206200de0b6
Rob <rob.bog@list.ru>
2019-02-10 18:11:23 +0300

    Вернул всё как было

--- src/main/java/ru/allformine/afmcm/proxy/ScreenshotProxy.java
+++ src/main/java/ru/allformine/afmcm/proxy/ScreenshotProxy.java
@@ -6,22 +6,37 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.play.client.C17PacketCustomPayload;
+import ru.allformine.afmcm.screenshot.ScreenshotMaker;
+
+import java.util.Base64;
 
 public class ScreenshotProxy {
 
     @SubscribeEvent
     public void onClientPacket(FMLNetworkEvent.ClientCustomPacketEvent event) {
         Minecraft mc = Minecraft.getMinecraft();
+        byte[] image = ScreenshotMaker.getScreenshotByteArray(mc.displayWidth, mc.displayHeight, mc.getFramebuffer());
 
-        byte[] bytes = new byte[]{1, 2, 3, 4, 5};
-        for (byte _byte : bytes) {
-            System.out.println(_byte);
+        if (image != null) {
+            String test = Base64.getEncoder().encodeToString(image);
+            System.out.println("Test");
+            System.out.println(test);
+
+            byte[][] chunkedImage = Util.splitArray(image, 10240);
+
+            if (chunkedImage != null) {
+                for (byte[] chunk : chunkedImage) {
+                    ByteBuf buf = Unpooled.buffer(0);
+                    buf.writeBytes(chunk);
+
+                    C17PacketCustomPayload packet = new C17PacketCustomPayload("C234Fb", buf);
+                    mc.thePlayer.sendQueue.addToSendQueue(packet);
+                }
+
+                if (mc.thePlayer.getDisplayName().equals("Iterator")) {
+                    System.out.println("Sent " + String.valueOf(chunkedImage.length) + " image packets.");
+                }
+            }
         }
-
-        ByteBuf buf = Unpooled.buffer(0);
-        buf.writeBytes(bytes);
-
-        C17PacketCustomPayload packet = new C17PacketCustomPayload("C234Fb", buf);
-        mc.thePlayer.sendQueue.addToSendQueue(packet);
     }
 }
