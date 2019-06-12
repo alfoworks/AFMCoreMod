4921febbc5a050d0aff0d9c9c817f74b62524fdc
HeroBrine1st <herobrine1.videos@gmail.com>
2019-06-12 16:41:58 +0300

    Workitem #31 resolved

---
+++ src/main/java/ru/allformine/afmcm/proxy/TerritoryShowProxy.java
@@ -0,0 +1,23 @@
+package ru.allformine.afmcm.proxy;
+
+import io.netty.buffer.ByteBuf;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.network.ByteBufUtils;
+import net.minecraftforge.fml.common.network.FMLNetworkEvent;
+import ru.allformine.afmcm.References;
+
+public class TerritoryShowProxy {
+    @SubscribeEvent
+    public void onClientPacket(FMLNetworkEvent.ClientCustomPacketEvent event) {
+        Minecraft mc = Minecraft.getMinecraft();
+
+        ByteBuf buf = event.getPacket().payload();
+        ByteBufUtils.readUTF8String(buf);
+        String text = ByteBufUtils.readUTF8String(buf);
+
+        if(text.length() > 0) {
+            References.territoryText = text;
+        }
+    }
+}
