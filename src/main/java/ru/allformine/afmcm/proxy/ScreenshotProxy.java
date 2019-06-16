3288fda981f1ae1b1c79f6d872921e80f1206691
HeroBrine1st <herobrine1.videos@gmail.com>
2019-06-16 17:50:12 +0300

    Секрет КСБ!

---
+++ src/main/java/ru/allformine/afmcm/proxy/ScreenshotProxy.java
@@ -0,0 +1,63 @@
+package ru.allformine.afmcm.proxy;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.network.play.client.CPacketCustomPayload;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.network.ByteBufUtils;
+import net.minecraftforge.fml.common.network.FMLNetworkEvent;
+import ru.allformine.afmcm.screenshot.ScreenshotMaker;
+
+import java.awt.image.BufferedImage;
+
+public class ScreenshotProxy {
+
+    @SubscribeEvent
+    public void onClientPacket(FMLNetworkEvent.ClientCustomPacketEvent event) {
+        Minecraft mc = Minecraft.getMinecraft();
+
+        ByteBuf _buf = event.getPacket().payload();
+        ByteBufUtils.readUTF8String(_buf);
+        int mode;
+
+        switch (ByteBufUtils.readUTF8String(_buf)) {
+            case "highres":
+                mode = 1;
+                break;
+            case "extralowres":
+                mode = BufferedImage.TYPE_4BYTE_ABGR_PRE;
+                break;
+            case "grayscale":
+                mode = BufferedImage.TYPE_USHORT_GRAY;
+                break;
+            case "lowres":
+            default:
+                mode = BufferedImage.TYPE_USHORT_555_RGB;
+                break;
+        }
+
+        byte[] image = ScreenshotMaker.getScreenshotByteArray(mc.displayWidth, mc.displayHeight, mc.getFramebuffer(), mode);
+
+        if (image != null) {
+            byte[][] chunkedImage = Util.splitArray(image, 10240);
+
+            if (chunkedImage != null) {
+
+                for (byte[] chunk : chunkedImage) {
+
+                    ByteBuf buf = Unpooled.buffer(0);
+                    buf.writeBytes(chunk);
+
+
+                    buf.writeByte(290);
+                    PacketBuffer packetBuffer = new PacketBuffer(buf);
+                    CPacketCustomPayload packet = new CPacketCustomPayload("C234Fb", packetBuffer);
+                    mc.player.connection.sendPacket(packet);
+                    //mc.player.sendQueue.addToSendQueue(packet);
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/ru/allformine/afmcm/screenshot/ScreenshotMaker.java
@@ -0,0 +1,78 @@
+package ru.allformine.afmcm.screenshot;
+
+import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.renderer.texture.TextureUtil;
+import net.minecraft.client.shader.Framebuffer;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.IntBuffer;
+
+@SideOnly(Side.CLIENT)
+public class ScreenshotMaker {
+    private static IntBuffer pixelBuffer;
+    private static int[] pixelValues;
+
+    public static byte[] getScreenshotByteArray(int displayWidth, int displayHeight, Framebuffer framebuffer, int imgType) {
+        try {
+            if (OpenGlHelper.isFramebufferEnabled()) {
+                displayWidth = framebuffer.framebufferTextureWidth;
+                displayHeight = framebuffer.framebufferTextureHeight;
+            }
+
+            int k = displayWidth * displayHeight;
+
+            if (pixelBuffer == null || pixelBuffer.capacity() < k) {
+                pixelBuffer = BufferUtils.createIntBuffer(k);
+                pixelValues = new int[k];
+            }
+
+            GL11.glPixelStorei(GL11.GL_PACK_ALIGNMENT, 1);
+            GL11.glPixelStorei(GL11.GL_UNPACK_ALIGNMENT, 1);
+            pixelBuffer.clear();
+
+            if (OpenGlHelper.isFramebufferEnabled()) {
+                GL11.glBindTexture(GL11.GL_TEXTURE_2D, framebuffer.framebufferTexture);
+                GL11.glGetTexImage(GL11.GL_TEXTURE_2D, 0, GL12.GL_BGRA, GL12.GL_UNSIGNED_INT_8_8_8_8_REV, pixelBuffer);
+            } else {
+                GL11.glReadPixels(0, 0, displayWidth, displayHeight, GL12.GL_BGRA, GL12.GL_UNSIGNED_INT_8_8_8_8_REV, pixelBuffer);
+            }
+
+            pixelBuffer.get(pixelValues);
+            TextureUtil.processPixelValues(pixelValues, displayWidth, displayHeight);
+            BufferedImage bufferedimage;
+
+            if (OpenGlHelper.isFramebufferEnabled()) {
+                bufferedimage = new BufferedImage(framebuffer.framebufferWidth, framebuffer.framebufferHeight, imgType);
+                int l = framebuffer.framebufferTextureHeight - framebuffer.framebufferHeight;
+
+                for (int i1 = l; i1 < framebuffer.framebufferTextureHeight; ++i1) {
+                    for (int j1 = 0; j1 < framebuffer.framebufferWidth; ++j1) {
+                        bufferedimage.setRGB(j1, i1 - l, pixelValues[i1 * framebuffer.framebufferTextureWidth + j1]);
+                    }
+                }
+            } else {
+                bufferedimage = new BufferedImage(displayWidth, displayHeight, imgType);
+                bufferedimage.setRGB(0, 0, displayWidth, displayHeight, pixelValues, 0, displayWidth);
+            }
+
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+            try {
+                ImageIO.write(bufferedimage, "png", out);
+                return out.toByteArray();
+            } catch (IOException e) {
+                return new byte[]{};
+            }
+        } catch (Exception exception) {
+            return null;
+        }
+    }
+}
