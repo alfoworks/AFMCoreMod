4c94ade689da02e28c59f8b657f328f79658305d
Iterator <rob.bog@list.ru>
2020-03-27 21:31:30 +0300

    Промежуточный коммит

--- src/main/java/ru/allformine/afmcm/handlers/event/MessagingPacketHandler.java
+++ src/main/java/ru/allformine/afmcm/handlers/event/MessagingPacketHandler.java
@@ -1,6 +1,8 @@
 package ru.allformine.afmcm.handlers.event;
 
 import io.netty.buffer.ByteBuf;
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.network.ByteBufUtils;
 import net.minecraftforge.fml.common.network.FMLNetworkEvent;
@@ -27,6 +29,8 @@ public class MessagingPacketHandler {
             default:
                 return;
         }
+
+        Minecraft.getMinecraft().player.sendMessage(new TextComponentString(messageText));
 
         MessageDispatcher.displayMessage(messageType, messageText);
     }
